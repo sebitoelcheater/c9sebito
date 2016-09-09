2a7cd7f186b33b4ac0748798ba4617583c8ec4c4
Sebastian Hernandez <seba.hg@me.com>
2016-09-09 00:43:51 +0000

    add controller logic for hola_mundo

--- myapp/test.py
+++ myapp/test.py
@@ -1,5 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-class DummyTest(TestCase):
-    def test_ci(self):
-        pass
+class AppTest(TestCase):
+    def test_200_route(self):
+        '''
+        Check if route is accessible
+        '''
+        #arrange
+        client = Client()
+        
+        #act
+        response = client.get("/hola")
+        
+        #assert
+        self.assertEquals(200, response.status_code)
--- myapp/urls.py
+++ myapp/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from myapp.views import hola_mundo
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^hola$', hola_mundo),
 ]
